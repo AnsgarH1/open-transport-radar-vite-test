113829c72fa5654675e40704c6a3437be602d82a
Timothy Tjong-Ayong <timothy.tjong-ayong@student.hs-rm.de>
2022-07-20 19:54:51 +0200

    feedback form responsive

--- src/pages/Feedback/Feedback.tsx
+++ src/pages/Feedback/Feedback.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
-import { Button, FormControl, FormLabel, FormErrorMessage,
-  FormHelperText, Heading, Input, Textarea, Select, VStack, Flex, Box, useColorModeValue } from '@chakra-ui/react'
+import {
+  Button, FormControl, FormLabel, FormErrorMessage,
+  FormHelperText, Heading, Input, Textarea, Select, VStack, Flex, Box, useColorModeValue
+} from '@chakra-ui/react'
 import Layout from '../../components/Layout/Layout';
 import { Field, Form, Formik, FormikProps } from "formik";
 import { CSSTransition } from 'react-transition-group';
@@ -68,96 +70,106 @@ function Feedback() {
           classNames="thankyou-message"
           unmountOnExit
         >
-          <Heading as='h2' size={["sm", "md", "xl", "2xl"]}>Vielen Dank für dein Feedback!</Heading>
-        </CSSTransition>   
-        
+          <Heading as='h2' size={["md", "xl", "2xl"]}>Vielen Dank für dein Feedback!</Heading>
+        </CSSTransition>
+
         <CSSTransition
-        in={showForm}
-        timeout={600}
-        classNames={'contact-form'}
-        appear
-        unmountOnExit
-        onExited={() => setShowMessage(true)}
+          in={showForm}
+          timeout={600}
+          classNames={'contact-form'}
+          appear
+          unmountOnExit
+          onExited={() => setShowMessage(true)}
         >
-          <Box bg={prime} p={6} boxShadow="lg" rounded="lg" w="40rem">
-            <Heading as='h2' size='xl'>Kontaktformular</Heading><br/>
-            <Formik 
+          <Box
+            w={["full", 'md']}
+            bg={prime}
+            p={[9, 10]}
+            mt={[20, '10vh']}
+            mx='3'
+            boxShadow="lg"
+            rounded="lg"
+          >
+            <Heading as='h2' size={["lg", "xl"]}>Kontaktformular</Heading><br />
+            <Formik
               initialValues={initialValues}
-              onSubmit={() => {console.log; setShowForm(false)}}
+              onSubmit={() => { console.log; setShowForm(false) }}
               validationSchema={ValidationSchema}
-              >
-              
-              {({errors, touched}) => (
+            >
+
+              {({ errors, touched }) => (
 
                 <Form>
-                  <VStack spacing={4} align="flex-start">
-                  
-                  <FormControl isRequired>
-                    <FormLabel htmlFor='firstname'>Vorname</FormLabel>
-                    <Field
-                        as={Input}
-                        id='firstname'
-                        placeholder='Vorname'
-                        name='firstname'
-                        />              
-                    {errors.firstname && touched.firstname && <FormHelperText>{errors.firstname}</FormHelperText>}
-                  </FormControl>
-                    
-     
-                  <FormControl isRequired >
-                    <FormLabel htmlFor='lastname'>Nachname</FormLabel>
-                    <Field
-                      as={Input}
-                      id='lastname'
-                      name='lastname'
-                      placeholder='Nachname'
-                    />
-                    {errors.lastname && touched.lastname && <FormHelperText>{errors.lastname}</FormHelperText>}
-                  </FormControl>
+                    <VStack spacing={4} align={['flex-start', 'center']} w='full'>
 
-                  <FormControl isRequired>
-                    <FormLabel htmlFor='email'>Email</FormLabel>
-                    <Field as={Input}
-                      id='email'
-                      name='email'
-                      type='email'
-                    />
-                    {errors.email && touched.email && <div>{errors.email}</div>}
-                  </FormControl>
+                      <FormControl isRequired>
+                        <FormLabel htmlFor='firstname'>Vorname</FormLabel>
+                        <Field
+                          as={Input}
+                          id='firstname'
+                          placeholder='Vorname'
+                          name='firstname'
+                        />
+                        {errors.firstname && touched.firstname && <FormHelperText>{errors.firstname}</FormHelperText>}
+                      </FormControl>
 
 
-                  <FormControl>
-                    <FormLabel htmlFor='issue'>Anliegen</FormLabel>
-                    <Field as={Select} id='issue' name='issue'>
-                      <option disabled selected>Was ist dein Anliegen?</option>
-                      <option>Es fehlen Verkerhrslinien bei mir</option>
-                      <option>Die ÖPNV Daten stimmen nicht</option>
-                      <option>Keine Ahnung tbh</option>
-                    </Field>
-                    {errors.issue && touched.issue && <FormHelperText>{errors.issue}</FormHelperText>}
-                  </FormControl>
+                      <FormControl isRequired >
+                        <FormLabel htmlFor='lastname'>Nachname</FormLabel>
+                        <Field
+                          as={Input}
+                          id='lastname'
+                          name='lastname'
+                          placeholder='Nachname'
+                        />
+                        {errors.lastname && touched.lastname && <FormHelperText>{errors.lastname}</FormHelperText>}
+                      </FormControl>
 
-                  <FormControl>
-                    <Field as={Textarea}
-                      name='text'
-                      id='text'
-                      placeholder='Schreibe uns dein Feedback'
-                      size='md'
-                    />
-                  {errors.text && touched.text && <FormHelperText>{errors.text}</FormHelperText>}
+                      <FormControl isRequired>
+                        <FormLabel htmlFor='email'>Email</FormLabel>
+                        <Field as={Input}
+                          id='email'
+                          name='email'
+                          type='email'
+                        />
+                        {errors.email && touched.email && <div>{errors.email}</div>}
+                      </FormControl>
 
-                  </FormControl>
 
-                  <Button
-                    bgColor={sec}
-                    color={prime}
-                    boxShadow="lg"
-                    size='md'
-                    type="submit"
-                  >
-                    Senden
-                  </Button>
-                  </VStack>
+                      <FormControl>
+                        <FormLabel htmlFor='issue'>Anliegen</FormLabel>
+                        <Field as={Select} id='issue' name='issue'>
+                          <option disabled selected>Was ist dein Anliegen?</option>
+                          <option>Es fehlen Verkerhrslinien bei mir</option>
+                          <option>Die ÖPNV Daten stimmen nicht</option>
+                          <option>Keine Ahnung tbh</option>
+                        </Field>
+                        {errors.issue && touched.issue && <FormHelperText>{errors.issue}</FormHelperText>}
+                      </FormControl>
+
+                      <FormControl>
+                        <Field as={Textarea}
+                          name='text'
+                          id='text'
+                          placeholder='Schreibe uns dein Feedback'
+                          size='md'
+                        />
+                        {errors.text && touched.text && <FormHelperText>{errors.text}</FormHelperText>}
+
+                      </FormControl>
+
+                      <Button
+                        w={['full', 'auto']}
+                        alignSelf='right'
+                        bgColor={sec}
+                        color={prime}
+                        boxShadow="lg"
+                        size='md'
+                        type="submit"
+                      >
+                        Senden
+                      </Button>
+                    </VStack>
                 </Form>
               )}
             </Formik>
