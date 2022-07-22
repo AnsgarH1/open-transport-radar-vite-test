c3566139a5a57f0e7d48a9e4edbc15f8ef7f3d7b
Ansgar Hoyer <ansgar.hoyer@student.hs-rm.de>
2022-07-22 18:47:54 +0200

    update locationcontext usage

--- src/components/DepartureBoard/departureHooks/useStations.tsx
+++ src/components/DepartureBoard/departureHooks/useStations.tsx
@@ -1,25 +1,16 @@
 import { createStandaloneToast, ToastId, useToast, UseToastOptions } from '@chakra-ui/react'
 import { useEffect, useRef, useState } from 'react'
 import { getStations } from '../../../api/stations'
+import { useLocationContext } from '../../../context/LocationContext'
 
 const useStations = () => {
 
     const toast = useToast()
-    const locationToastId = useRef<ToastId>()
     const stationsToastId = useRef<ToastId>()
 
-    const showLocationToast = () => {
-        locationToastId.current = toast({ status: "info", description: "Lade Standort..", duration: null, isClosable: true })
-    }
-
-    const updateLocationToast = (options: UseToastOptions) => {
-        if (locationToastId.current) {
-            toast.update(locationToastId.current, options)
-        }
-    }
 
     const showStationsToast = () => {
-        stationsToastId.current = toast({ status: "info", description: "Lade Haltestellen..", duration: null, isClosable: true })
+        stationsToastId.current = toast({ status: "loading", title: "Lade Haltestellen..", duration: null, isClosable: true })
     }
     const updateStationToast = (options: UseToastOptions) => {
         if (stationsToastId.current) {
@@ -31,43 +22,41 @@ const useStations = () => {
 
     const [isLoadingGeoLocation, setLoadingGeoLocation] = useState(false)
     const [isLoadingStations, setLoadingStations] = useState(false)
-    const [currentCoords, setCoords] = useState<GeolocationCoordinates>()
     const [nearbyStations, setNearbyStations] = useState<Hafas_Stations.Station[]>([])
-
-
+    const { currentLocation } = useLocationContext()
+    const [errorDisplayText, setErrorDisplayText] = useState("⏲warte auf Standort..")
     function loadStations(lat: number, long: number) {
+        setErrorDisplayText("🚏 lade Haltestellen..")
         showStationsToast()
         setLoadingStations(true)
         getStations({ lat, long })
             .then(stations => {
-                updateStationToast({ status: "success", description: "Haltestellen gefunden!", duration: 2000 })
-                if (stations) setNearbyStations(stations)
+                updateStationToast({ status: "success", title: "Haltestellen gefunden!", duration: 2000 })
+                if (stations && stations?.length > 0) {
+                    setErrorDisplayText("hier sollte eigentlich keine Fehlermeldung stehen👻")
+                    setNearbyStations(stations)
+                } else {
+                    setErrorDisplayText("Es konnten keine Haltestellen geladen werden!🕳")
+
+                }
+            }).catch(error => {
+                updateStationToast({ status: "error", title: "Haltestellen konnten nicht geladen werden!", duration: 2000 })
+                setErrorDisplayText("❌Es ist Fehler aufgetreten")
             }).finally(() => setLoadingStations(false))
     }
 
-    /** Fetching Browser Location **/
-    useEffect(() => {
-        if ("geolocation" in navigator) {
-            setLoadingGeoLocation(true)
-            showLocationToast()
-            navigator.geolocation.getCurrentPosition(position => {
-                updateLocationToast({ status: "success", description: "Standort gefunden!", duration: 2000 })
-                //console.log("found position,", position.coords)
-                setCoords(position.coords)
-                /** Get Nearby Stations with found coordinates */
-                loadStations(position.coords.latitude, position.coords.longitude)
-            }, (error) => {
-                updateLocationToast({ status: "error", title: "Standort-Feler", description: error.message, duration: 5000 })
 
-            })
+    useEffect(() => {
+        if (currentLocation) {
+            loadStations(currentLocation.coords.latitude, currentLocation.coords.longitude)
         }
-    }, [])
+    }, [currentLocation])
 
     return {
         nearbyStations: nearbyStations || undefined,
         loadStations,
-        currentCoords,
-        isLoadingStations
+        isLoadingStations,
+        errorDisplayText
 
     }
 }
