76c3eef8b6957e148334e7a076b19b95d44eb365
Ansgar Hoyer <ansgar.hoyer@student.hs-rm.de>
2022-08-11 18:43:02 +0200

    update pwa stuff

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,59 @@ import { defineConfig } from "vite";
 
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
+import { icons } from "react-icons";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets:["assets/favicon.ico","assets/apple-touch-icon.png", "assets/masked-icon.png"],
+      manifest: {
+        name: "Public-Transport-Radar",
+        short_name: "Public-Transport-Radar",
+        display: "standalone",
+        description: "Finde deine nächsten Abfahrten in wenigen Sekunden!",
+        lang: "german",
+        theme_color: "#789E9E",
+        background_color: "#1A202C",
+        orientation: "portrait",
+        icons: [
+          {
+            src: "assets/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "assets/pwa-192x192.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "assets/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        screenshots: [
+          {
+            src: "assets/screenshots/Screenshot1.PNG",
+            sizes: "389x842",
+            type: "image/png",
+          },
+          {
+            src: "assets/screenshots/Screenshot2.PNG",
+            sizes: "389x842",
+            type: "image/png",
+          },
+          {
+            src: "assets/screenshots/Screenshot3.PNG",
+            sizes: "389x842",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
