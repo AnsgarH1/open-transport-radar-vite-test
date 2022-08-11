528dd18afac366b5ad5edc8d263572c7410d41b2
Ansgar Hoyer <ansgar.hoyer@student.hs-rm.de>
2022-08-11 18:43:18 +0200

    update vite pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from "vite";
 
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
-import { icons } from "react-icons";
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [
     react(),
